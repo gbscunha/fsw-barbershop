f9ca686b039a0e930eb434c93ca1e724554afe5f
Gabriel Cunha <gabrielalternativo09@gmail.com>
2024-09-23 10:08:10 -0300

    feat: add BookingItem components

---
+++ app/_constants/search.ts
@@ -0,0 +1,25 @@
+interface QuickSearchOptions {
+    imageUrl: string,
+    title: string
+  }
+  
+  export const quickSearchOptions: QuickSearchOptions[] = [{
+    imageUrl: "/barba.svg",
+    title: "Barba"
+  },{
+    imageUrl: "/cabelo.svg",
+    title: "Cabelo"
+  },{
+    imageUrl: "/acabamento.svg",
+    title: "Acabamento"
+  },{
+    imageUrl: "/massagem.svg",
+    title: "Massagem"
+  },{
+    imageUrl: "/sobrancelha.svg",
+    title: "Sobrancelha"
+  },{
+    imageUrl: "hidratacao.svg",
+    title: "Hidratação"
+  }
+  ]
