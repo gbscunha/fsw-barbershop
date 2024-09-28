6fe9acf0076de632ad1cb054fe7e70445d7d5738
Gabriel Cunha <gabrielalternativo09@gmail.com>
2024-09-28 09:53:05 -0300

    feat: add profile image

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -33,7 +33,7 @@ const Header = () => {
 
             <div className="flex items-center gap-3 border-b border-solid py-5">
               <Avatar>
-                <AvatarImage src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YXZhdGFyfGVufDB8fDB8fHww" />
+                <AvatarImage src="./profile.jpeg" />
               </Avatar>
 
               <div>
